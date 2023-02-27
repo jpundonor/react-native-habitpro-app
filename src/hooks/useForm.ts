a91bc654205c7ab12ceb59f6c01bf71a6fb2079a
Javier Rojas <jardlml1209@gmail.com>
2023-02-27 19:12:58 -0300

    Revert "Revert "The functionality of the form in the Login has been momentarily removed""
    This reverts commit 1da5e7ef542a1587c332c2f5dffc9612837fdd86.

--- src/hooks/useForm.ts
+++ src/hooks/useForm.ts
@@ -1,90 +1,92 @@
 import {useState} from 'react';
 
-interface FormState {
-  [key: string]: string;
-}
+export const useForm = (initialForm = {}) => {
+  const [formState, setFormState] = useState(initialForm);
 
-interface FormValidator {
-  [key: string]: (value: string) => string | undefined;
-}
-
-interface UseFormOptions {
-  initialValues: FormState;
-  onSubmit: (values: FormState) => void;
-  validators?: FormValidator;
-}
-
-interface UseFormReturn {
-  values: FormState;
-  errors: FormState;
-  handleChange: (fiel: string, value: string) => void;
-  handleSubmit: () => void;
-}
-
-export const useForm = ({
-  initialValues,
-  onSubmit,
-  validators = {},
-}: UseFormOptions): UseFormReturn => {
-  const [values, setValues] = useState<FormState>(initialValues);
-  const [errors, setErrors] = useState<FormState>({});
-
-  const handleChange = (field: string, value: string | undefined) => {
-    setValues(prevValues => ({
-      ...prevValues,
-      [field]: value?.toString() || '',
-    }));
-    // if (validators[field]) {
-    //   const error = validators[field](value?.toString() || '');
-    //   setErrors((prevErrors) => ({
-    //     ...prevErrors,
-    //     [field]: error,
-    //   }));
-    // }
-  };
-
-  const handleSubmit = () => {
-    const newErrors = {} as FormState;
-
-    Object.keys(values).forEach(key => {
-      if (validators[key]) {
-        const error = validators[key](values[key]);
-
-        if (error) {
-          newErrors[key] = error;
-        }
-      }
+  const onInputChange = ({target}:{target: any}) => {
+    const {name, value} = target;
+    setFormState({
+      ...formState,
+      [name]: value,
     });
-
-    if (Object.keys(newErrors).length > 0) {
-      setErrors(newErrors);
-    } else {
-      onSubmit(values);
-    }
   };
 
-  return {values, errors, handleChange, handleSubmit};
+  const onResetForm = () => {
+    setFormState(initialForm);
+  };
+
+  return {
+    ...formState,
+    formState,
+    onInputChange,
+    onResetForm,
+  };
 };
 
-// export const useForm = (initialForm = {}) => {
-//   const [formState, setFormState] = useState(initialForm);
 
-//   const onInputChange = ({target}) => {
-//     const {name, value} = target;
-//     setFormState({
-//       ...formState,
-//       [name]: value,
+// interface FormState {
+//   [key: string]: string;
+// }
+
+// interface FormValidator {
+//   [key: string]: (value: string) => string | undefined;
+// }
+
+// interface UseFormOptions {
+//   initialValues: FormState;
+//   onSubmit: (values: FormState) => void;
+//   validators?: FormValidator;
+// }
+
+// interface UseFormReturn {
+//   values: FormState;
+//   errors: FormState;
+//   handleChange: (fiel: string, value: string) => void;
+//   handleSubmit: () => void;
+// }
+
+// export const useForm = ({
+//   initialValues,
+//   onSubmit,
+//   validators = {},
+// }: UseFormOptions): UseFormReturn => {
+//   const [values, setValues] = useState<FormState>(initialValues);
+//   const [errors, setErrors] = useState<FormState>({});
+
+//   const handleChange = (field: string, value: string | undefined) => {
+//     setValues(prevValues => ({
+//       ...prevValues,
+//       [field]: value?.toString() || '',
+//     }));
+//     // if (validators[field]) {
+//     //   const error = validators[field](value?.toString() || '');
+//     //   setErrors((prevErrors) => ({
+//     //     ...prevErrors,
+//     //     [field]: error,
+//     //   }));
+//     // }
+//   };
+
+//   const handleSubmit = () => {
+//     const newErrors = {} as FormState;
+
+//     Object.keys(values).forEach(key => {
+//       if (validators[key]) {
+//         const error = validators[key](values[key]);
+
+//         if (error) {
+//           newErrors[key] = error;
+//         }
+//       }
 //     });
+
+//     if (Object.keys(newErrors).length > 0) {
+//       setErrors(newErrors);
+//     } else {
+//       onSubmit(values);
+//     }
 //   };
 
-//   const onResetForm = () => {
-//     setFormState(initialForm);
-//   };
-
-//   return {
-//     ...formState,
-//     formState,
-//     onInputChange,
-//     onResetForm,
-//   };
+//   return {values, errors, handleChange, handleSubmit};
 // };
+
