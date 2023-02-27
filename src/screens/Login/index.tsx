7e60771b2eb017f2e898d6f90bfbd665b03b7144
Javier Rojas <jardlml1209@gmail.com>
2023-02-27 19:00:29 -0300

    The functionality of the form in the Login has been momentarily removed

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Button, Text, TextInput, TouchableOpacity, View} from 'react-native';
-import {useForm} from '../../hooks';
+// import {useForm} from '../../hooks';
 import {styles} from './styles';
 
 // interface LoginValues {
@@ -8,36 +8,39 @@ import {styles} from './styles';
 //   password: string;
 // }
 
-const validators = {
-  email: (value: string) => {
-    if (!value) {
-      return 'Email is required';
-    }
-    if (!/\S+@\S+\.\S+/.test(value)) {
-      return 'Invalid email format';
-    }
-    return ''; // Devuelve una cadena vacía si el valor es válido
-  },
-  password: (value: string) => {
-    if (!value) {
-      return 'Password is required';
-    }
-    if (value.length < 6) {
-      return 'Password must be at least 6 characters long';
-    }
-    return ''; // Devuelve una cadena vacía si el valor es válido
-  },
-};
+// const validators = {
+//   email: (value: string) => {
+//     if (!value) {
+//       return 'Email is required';
+//     }
+//     if (!/\S+@\S+\.\S+/.test(value)) {
+//       return 'Invalid email format';
+//     }
+//     return ''; // Devuelve una cadena vacía si el valor es válido
+//   },
+//   password: (value: string) => {
+//     if (!value) {
+//       return 'Password is required';
+//     }
+//     if (value.length < 6) {
+//       return 'Password must be at least 6 characters long';
+//     }
+//     return ''; // Devuelve una cadena vacía si el valor es válido
+//   },
+// };
 
 export const Login = ({navigation}: {navigation: any}) => {
-  const {values, errors, handleChange, handleSubmit} = useForm({
-    initialValues: {email: '', password: ''},
-    onSubmit: value => {
-      navigation.navigate('Home');
-      console.log(value);
-    },
-    validators,
-  });
+  // const {values, errors, handleChange, handleSubmit} = useForm({
+  //   initialValues: {email: '', password: ''},
+  //   onSubmit: value => {
+  //     navigation.navigate('Home');
+  //     console.log(value);
+  //   },
+  //   validators,
+  // });
+  const handleSubmit = () => {
+    navigation.navigate('Home');
+  };
 
   const onGoogleSignIn = () => {
     console.log('onGoogleSignIn');
@@ -48,33 +51,33 @@ export const Login = ({navigation}: {navigation: any}) => {
       <Text style={styles.label}>Correo</Text>
       <TextInput
         style={styles.input}
-        value={values.email}
-        onChangeText={value => handleChange('email', value)}
+        // value={values.email}
+        // onChangeText={value => handleChange('email', value)}
         placeholder="Email"
         autoCapitalize="none"
         autoCorrect={false}
       />
-      {errors.email && <Text>{errors.email}</Text>}
+      {/* {errors.email && <Text>{errors.email}</Text>} */}
 
       <Text style={styles.label}>Contraseña</Text>
       <TextInput
         style={styles.input}
-        value={values.password}
-        onChangeText={value => handleChange('password', value)}
+        // value={values.password}
+        // onChangeText={value => handleChange('password', value)}
         placeholder="Password"
         secureTextEntry={true}
         autoCapitalize="none"
         autoCorrect={false}
       />
-      {errors.password && <Text>{errors.password}</Text>}
+      {/* {errors.password && <Text>{errors.password}</Text>} */}
       <TouchableOpacity style={styles.button} onPress={handleSubmit}>
         <Text style={styles.buttonText}>Ingresar</Text>
       </TouchableOpacity>
-      <Button title="O inicia sesión con Google" onPress={onGoogleSignIn} />
+      <Button title="Or login with Google" onPress={onGoogleSignIn} />
       <View>
         <Text style={styles.label}>¿No tienes una cuenta?</Text>
         <Button
-          title="Registrate aquí"
+          title="Sign up here"
           onPress={() => navigation.navigate('Register')}
         />
       </View>
